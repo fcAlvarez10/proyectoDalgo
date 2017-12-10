1e1199988f2e16a2458c7d2f082524e7020061d1
Fabian Alvarez <fc.alvarez10@uniandes.edu.co>
2017-12-10 11:24:35 -0500

    A funcionando

--- src/prodal/ProblemaA.java
+++ src/prodal/ProblemaA.java
@@ -3,21 +3,40 @@ package prodal;
 public class ProblemaA {
 	public static void main(String[] args)throws Exception{
 		ProblemaA instancia= new ProblemaA();
-		System.out.println(instancia.convolucionPonderada(10,0, 1, 1, 1));
+		System.out.println(instancia.convolucionPonderada(15,2, 0.7, 0.45, 0.55));
 	}
 
-	public double convolucionPonderada(int n,int A, int B, int C, int D){
+	public double convolucionPonderada(int n,double A, double B, double C, double D){
 
-		double cp=0,r0=0,r1=0,cp1=0,cp2=0,cp3,aux;
+		double cp=0,r0=0,r1=0,cp1=0,cp2=0,cp3=0,aux;
 		int i=0,j=n;
 		while(i<j){
 			r0=A;
 			r1=B;
+			if(i==0){
+				cp1=r0;
+			}else if(i==1){
+				cp1=r1;
+			}
+			for(int k=2;k<=j;k++){
+				aux=C*r0+D*r1;
+				r0=r1;
+				r1=aux;
+				if(k==i){
+					cp1=r1;
+				}
+				if(k==j){
+					cp2=r1;
+				}
+				if(k==n/2 && n%2==0){
+					cp3=r1;
+				}
+			}
+			cp+=2*cp1*cp2+cp3;
 			cp1=0;
 			cp2=0;
 			cp3=0;
-
-			
+			aux=0;
 			i++;
 			j--;
 		}
